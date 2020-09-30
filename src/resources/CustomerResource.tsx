135ad99493e04e7fa5ede3a2a73ac7e6be3289b7
Tam <tamvq@vng.com.vn>
2020-09-30 16:38:32 +0700

    add alert time option

--- src/resources/CustomerResource.tsx
+++ src/resources/CustomerResource.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
-import { useMediaQuery, Button } from '@material-ui/core';
-import { Resource, ListGuesser, SimpleForm, TextInput, Create, Edit, DateInput, NumberInput, List, Datagrid, TextField, EmailField, DateField, Filter, ReferenceField, email } from 'react-admin';
+import React, { cloneElement, useState, useEffect } from 'react';
+import { useMediaQuery, Button, Select, FormControl, InputLabel, Divider } from '@material-ui/core';
+import { Resource, ListGuesser, SimpleForm, TextInput, Create, Edit, DateInput, NumberInput, List, Datagrid, TextField, EmailField, DateField, Filter, ReferenceField, email, TopToolbar, sanitizeListRestProps, CreateButton, ExportButton, useListContext } from 'react-admin';
 
 import { CountField, ScannerField } from '../components';
+
+function useLocalStorage(key, initialValue) {
+    // State to store our value
+    // Pass initial state function to useState so logic is only executed once
+    const [storedValue, setStoredValue] = useState(() => {
+        try {
+            // Get from local storage by key
+            const item = window.localStorage.getItem(key);
+            // Parse stored json or if none return initialValue
+            return item ? JSON.parse(item) : initialValue;
+        } catch (error) {
+            // If error also return initialValue
+            console.log(error);
+            return initialValue;
+        }
+    });
+
+    // Return a wrapped version of useState's setter function that ...
+    // ... persists the new value to localStorage.
+    const setValue = value => {
+        try {
+            // Allow value to be a function so we have same API as useState
+            const valueToStore =
+                value instanceof Function ? value(storedValue) : value;
+            // Save state
+            setStoredValue(valueToStore);
+            // Save to local storage
+            window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        } catch (error) {
+            // A more advanced implementation would handle the error case
+            console.log(error);
+        }
+    };
+
+    return [storedValue, setValue];
+}
+
 
 const FilterView = (props) => (
     <Filter {...props}>
@@ -13,11 +50,72 @@ const FilterView = (props) => (
     </Filter>
 );
 
+const AlertButton = (props) => {
+    const [time, setTime] = useLocalStorage('alert-customer', 7);
+
+    return (
+        <FormControl variant="outlined" size="small" color="primary" style={{ marginRight: 10 }}>
+            <InputLabel htmlFor="outlined-age-native-simple">Time to alert</InputLabel>
+            <Select native label="Time to alert" onChange={e => setTime(e.target.value)} value={time}>
+                <option value={7}>1 week</option>
+                <option value={14}>2 weeks</option>
+                <option value={30}>1 month</option>
+            </Select>
+        </FormControl>
+    )
+}
+
+const ActionView = (props) => {
+    const {
+        className,
+        exporter,
+        filters,
+        maxResults,
+        ...rest
+    } = props;
+
+    const {
+        currentSort,
+        resource,
+        displayedFilters,
+        filterValues,
+        hasCreate,
+        basePath,
+        selectedIds,
+        showFilter,
+        total
+    } = useListContext();
+
+    return (
+        <TopToolbar className={className} {...sanitizeListRestProps(rest)}>
+            <AlertButton />
+
+            <Divider orientation="vertical" flexItem />
+
+            {filters && cloneElement(filters, {
+                resource,
+                showFilter,
+                displayedFilters,
+                filterValues,
+                context: 'button',
+            })}
+            <CreateButton basePath={basePath} />
+            <ExportButton
+                disabled={total === 0}
+                resource={resource}
+                sort={currentSort}
+                filterValues={filterValues}
+                maxResults={maxResults}
+            />
+        </TopToolbar>
+    );
+};
+
 const ListView = props => {
     const isSmall = useMediaQuery((theme: any) => theme.breakpoints.down('sm'));
 
     return (
-        <List filters={<FilterView />} {...props}>
+        <List filters={<FilterView />} actions={<ActionView />} {...props}>
             <Datagrid>
                 <TextField source="id" />
                 <TextField source="name" />
